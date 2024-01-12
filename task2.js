c48a262bacb0dddd0eabe05647286cb4cc9986ae
JohanDevia <johandeviag@gmail.com>
2024-01-11 20:01:38 -0500

    Update task2.js
    task 2 finished and added

--- task2.js
+++ task2.js
@@ -6,3 +6,13 @@ const ingredients = [
   "Herbs",
   "Condiments",
 ];
+const ul = document.querySelector("#ingredients");
+
+for (let i = 0; i < ingredients.length; i++) {
+  const li = document.createElement("li");
+
+  const text = document.createTextNode(ingredients[i]);
+  li.appendChild(text);
+  li.classList.add("item");
+  ul.appendChild(li);
+}
