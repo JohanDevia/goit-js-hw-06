f5a90aaebf7ccbb3a728c3bdaf000c9eac73c581
JohanDevia <johandeviag@gmail.com>
2024-01-14 09:49:19 -0500

    corrections task 8
    id selector is changed to class selector to remove error in console, styles are added

--- task8.js
+++ task8.js
@@ -1,4 +1,4 @@
-const loginForm = document.getElementById("loginForm");
+const loginForm = document.querySelector(".login-form");
 
 loginForm.addEventListener("submit", (event) => {
   event.preventDefault();
